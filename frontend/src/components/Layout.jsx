aadd30c5678f4801479679a119e9afaedfe23767
razxxy12 <razxxy12@gmail.com>
2026-06-05 04:53:54 +0700

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,9 +5,9 @@ import Topbar from './Topbar';
 const Layout = () => (
   <div className="flex h-screen overflow-hidden bg-gray-100">
     <Sidebar />
-    <div className="flex-1 flex flex-col overflow-hidden">
+    <div className="flex-1 flex flex-col overflow-hidden min-w-0">
       <Topbar />
-      <main className="flex-1 overflow-y-auto p-6">
+      <main className="flex-1 overflow-y-auto p-4 md:p-6">
         <Outlet />
       </main>
     </div>
